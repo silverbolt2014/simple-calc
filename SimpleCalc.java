a6bcb46c4d36906873f1c419bbbdb7c4fe8725f7
Silver Bolt <silverbolt2014@gmail.com>
2015-11-26 08:26:31 -0500

    Add functionality for other operations

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -11,36 +11,62 @@ public class SimpleCalc {
     JButton goButton;
     JButton resetButton;
     JComboBox<String> selectOperationComboBox;
+    Font defaultFont = new Font("Serif", Font.BOLD, 20);
 
     public static void main(String[] args) {
         SimpleCalc simpleCalcGui = new SimpleCalc();
-        simpleCalcGui.go();
+        simpleCalcGui.initialize();
     }
 
-    public void go(){
+    /**
+    * This method mainly sets up the gui components.
+    */
+    public void initialize(){
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel num1Label = new JLabel("Number 1");
+        num1Label.setFont(defaultFont);
+        
         JLabel num2Label = new JLabel("Number 2");
+        num2Label.setFont(defaultFont);
+        
         JLabel resultsLabel = new JLabel("Results");
+        resultsLabel.setFont(defaultFont);
 
         num1TextField = new JTextField();
+        num1TextField.setFont(defaultFont);
+        num1TextField.setHorizontalAlignment(JTextField.CENTER);
+
         num2TextField = new JTextField();
+        num2TextField.setFont(defaultFont);
+        num2TextField.setHorizontalAlignment(JTextField.CENTER);
+
         resultsTextField = new JTextField();
+        resultsTextField.setFont(defaultFont);
         resultsTextField.setEditable(false);
+        resultsTextField.setHorizontalAlignment(JTextField.CENTER);
         
         String[] operationStrings = {"Add", "Minus", "Multiply", "Divide"};
         selectOperationComboBox = new JComboBox<String>(operationStrings);
+        selectOperationComboBox.setFont(defaultFont);
+        ((JLabel)selectOperationComboBox.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
         
         goButton = new JButton("GO!");
         goButton.addActionListener( new GoButtonActionListener() );
+        goButton.setFont(defaultFont);
+        //goButton.setHorizontalAlignment(SwingConstants.CENTER);
 
         resetButton = new JButton("Reset");
         resetButton.addActionListener( new ResetButtonActionListener() );
+        resetButton.setFont(defaultFont);
+        //resetButton.setHorizontalAlignment(SwingConstants.LEFT);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
         
         mainPanel.add(num1Label);
         mainPanel.add(num1TextField);
@@ -53,60 +79,78 @@ public class SimpleCalc {
         mainPanel.add(resultsLabel);
         mainPanel.add(resultsTextField);
 
-        mainPanel.add(goButton);
-        mainPanel.add(resetButton);
+        mainPanel.add(buttonPanel);
+
+        buttonPanel.add(goButton);
+        buttonPanel.add(resetButton);
 
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        frame.setSize(300, 300);
+        frame.setSize(300, 500);// w x h
         frame.setVisible(true);        
     }
     
     public class GoButtonActionListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             //System.out.println("this is go button actionlistener event");
-
-            String num1String = "-1";
-            String num2String = "-1";
-
+            String num1String = null;
+            String num2String = null;
+            boolean canPrepareCalculation = false;
             try {
                 num1String = num1TextField.getText();
                 num2String = num2TextField.getText();
-                System.out.println("num1String = " + num1String);
-                System.out.println("num2String = " + num2String);
+                canPrepareCalculation = true;
+                //System.out.println("num1String = " + num1String);
+                //System.out.println("num2String = " + num2String);
+
             } catch (NullPointerException exception) {
                 exception.printStackTrace();
             }
 
-
-            int num1;
-            int num2;
-            try {
-                num1 = Integer.parseInt(num1String);
-                num2 = Integer.parseInt(num2String);
-                String operationToPerform = (String) selectOperationComboBox.getSelectedItem();
-                performCalculation(num1, num2, operationToPerform);
-            } catch (NumberFormatException e) {
-                System.out.println("There was a problem with one or more number inputted by the user");
+            if (canPrepareCalculation) {
+                int num1;
+                int num2;
+                try {
+                    num1 = Integer.parseInt(num1String);
+                    num2 = Integer.parseInt(num2String);
+                    String operationToPerform = (String) selectOperationComboBox.getSelectedItem();
+                    performCalculation(num1, num2, operationToPerform);
+                } catch (NumberFormatException e) {
+                    System.out.println("There was a problem with the input provided by the user. Please use valid numbers.");
+                }
             }
         }
     }
 
     void performCalculation(double n1, double n2, String operation) {
-        double result = 0;
+        double result = Double.NaN;
         switch (operation.toLowerCase()) {
              case "add":
-                System.out.println("Add Selected");
+                //System.out.println("Add Selected");
                 result = n1 + n2;
                 break;
             case "minus":
-                System.out.println("Minus Selected");
+                //System.out.println("Minus Selected");
+                result = n1 - n2;
                 break;
             case "multiply":
-                System.out.println("Multiply Selected");
+                //System.out.println("Multiply Selected");
+                result = n1 * n2;
                 break;
             case "divide":
-                System.out.println("Divide Selected");
+                //System.out.println("Divide Selected");
+                if (n2 != 0) {
+                    result = n1 / n2;
+                } else {
+                    JLabel errorMessageLabel = new JLabel("Unable to perform operation.  Invalid divisor.");
+                    errorMessageLabel.setFont(defaultFont);
+                    JOptionPane pane = new  JOptionPane(errorMessageLabel, JOptionPane.ERROR_MESSAGE, JOptionPane.DEFAULT_OPTION);
+                    JDialog dialog = pane.createDialog(frame, "Error");
+                    dialog.setVisible(true);
+                }
                 break;
+            default:
+                System.out.println("We should never get here.  Default case statement");
+            break;
         }
 
         resultsTextField.setText(result + "");
@@ -114,8 +158,10 @@ public class SimpleCalc {
 
     public class ResetButtonActionListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            System.out.println("this is reset button actionlistener event");
-
+            //System.out.println("this is reset button actionlistener event");
+            num1TextField.setText("");
+            num2TextField.setText("");
+            resultsTextField.setText("");
         }
     }    
 }
